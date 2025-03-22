648beab2229c20cadbf81de69280653989138259
Ralph Bergmann <ralph@the4thfloor.eu>
2025-03-22 15:15:01 +0100

    add ViewModelBuilder

--- examples/flutter_demo/lib/src/features/home/my_home_page.dart
+++ examples/flutter_demo/lib/src/features/home/my_home_page.dart
@@ -44,6 +44,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final viewModelBuilder = MyHomePageViewModelBuilder();
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -55,6 +56,17 @@ class _MyHomePageState extends State<MyHomePage> {
           children: <Widget>[
             const Text(
               'You have pushed the button this many times:',
+            ),
+            viewModelBuilder(
+              builder: (context, viewModel, child) {
+                return Column(
+                  children: [
+                    const Text('foo'),
+                    if (child != null) child,
+                  ],
+                );
+              },
+              child: const Text('bla'),
             ),
             ListenableBuilder(
               listenable: viewModel,
--- examples/flutter_demo/lib/src/features/home/my_home_page_view_model.dart
+++ examples/flutter_demo/lib/src/features/home/my_home_page_view_model.dart
@@ -19,3 +19,73 @@ class MyHomePageViewModel extends ChangeNotifier {
     notifyListeners();
   }
 }
+
+class MyHomePageViewModelProvider extends Provider<MyHomePageViewModel> {
+  @override
+  MyHomePageViewModel get() {
+    return MyHomePageViewModel(repository: repository);
+  }
+}
+
+typedef ViewModelWidgetBuilder<T extends ChangeNotifier> = Widget Function(
+  BuildContext context,
+  T viewModel,
+  Widget? child,
+);
+
+final class MyHomePageViewModelBuilder {
+  const MyHomePageViewModelBuilder();
+
+  ViewModelBuilder<MyHomePageViewModel> call({
+    required ViewModelWidgetBuilder<MyHomePageViewModel> builder,
+    Widget? child,
+  }) {
+    return ViewModelBuilder<MyHomePageViewModel>(
+      viewModelProvider: MyHomePageViewModelProvider(),
+      builder: builder,
+    );
+  }
+}
+
+final class ViewModelBuilder<T extends ChangeNotifier> extends StatefulWidget {
+  const ViewModelBuilder({
+    super.key,
+    required this.viewModelProvider,
+    required this.builder,
+    this.child,
+  });
+
+  final Provider<T> viewModelProvider;
+  final ViewModelWidgetBuilder<T> builder;
+  final Widget? child;
+
+  @override
+  State<ViewModelBuilder<T>> createState() => _ViewModelBuilderState<T>();
+}
+
+final class _ViewModelBuilderState<T extends ChangeNotifier> extends State<ViewModelBuilder<T>> {
+  late final T _viewModel;
+
+  @override
+  void initState() {
+    super.initState();
+    _viewModel = widget.viewModelProvider.get();
+  }
+
+  @override
+  void dispose() {
+    _viewModel.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => ListenableBuilder(
+        listenable: _viewModel,
+        builder: (context, child) => widget.builder(
+          context,
+          _viewModel,
+          child,
+        ),
+        child: widget.child,
+      );
+}
